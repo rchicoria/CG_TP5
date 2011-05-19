9c55e274b2fcd61ecf52d473a4e432d4c68bdef4
Ricardo Lopes <ricardorpl@gmail.com>
2011-05-19 16:24:18 +0100

    agora esta tudo bem

--- TP5.cpp
+++ TP5.cpp
@@ -13,9 +13,9 @@ Rui Pedro da Cunha Santos Chicória 2008115099
 //--------------------------------- Definir cores
 #define AZUL     0.0, 0.0, 1.0, 1.0
 #define VERMELHO 1.0, 0.0, 0.0, 1.0
-#define AMARELO  1.0, 1.0, 0.9, 1.0
-#define VERDE    0.0, 1.0, 0.0, 1.0
-#define LARANJA  1.0, 0.5, 0.1, 0.5
+#define AMARELO  1.0, 1.0, 0.5, 1.0
+#define VERDE    0.0, 1.0, 0.0, alpha
+#define LARANJA  1.0, 0.5, 0.1, alpha
 #define WHITE    1.0, 1.0, 1.0, 1.0
 #define BLACK    0.0, 0.0, 0.0, 1.0
 #define GRAY     0.92, 0.92, 0.92, 1.0
@@ -38,6 +38,7 @@ GLfloat  obsP[] = {raio/5, 0.3, raio};   //posicao inicial do observador
 
 //------------------------------------------------------------- Esferas
 GLfloat  incar   = 0.5; //rotaçao da esfera
+GLfloat  alpha	 = 0.5;
 GLfloat angRot = 0;
 
 GLfloat   sphereP1[] = {0 ,0, 0}; //Posicao do sphere
@@ -55,6 +56,7 @@ void init(void)
 	glEnable(GL_TEXTURE_2D);
 	glEnable(GL_DEPTH_TEST);
 	glClearColor(AMARELO);
+	glEnable(GL_CULL_FACE);
 }
 
 
@@ -65,26 +67,29 @@ void resizeWindow(GLsizei w, GLsizei h)
 	glutPostRedisplay();
 }
 
+void sphere1()
+{
+	glColor4f(VERDE);
+	glTranslatef(sphereP1[0], sphereP1[1], sphereP1[2]);
+	glutSolidSphere(sphereT1[0], sphereT1[1], sphereT1[2]);
+}
+
+void sphere2()
+{
+	glColor4f(LARANJA);
+	glRotatef(angRot,0,1,0);
+	glTranslatef(sphereP2[0], sphereP2[1], sphereP2[2]);
+	glutSolidSphere(sphereT2[0], sphereT2[1], sphereT2[2]);
+}
+
 void drawScene(){
 	
-	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Sphere
-	glDisable(GL_BLEND);
-	glBlendFunc (GL_ONE, GL_ZERO);
-	glPushMatrix();
-		glColor4f(VERDE);
-		glTranslatef(sphereP1[0], sphereP1[1], sphereP1[2]);
-		glutSolidSphere(sphereT1[0], sphereT1[1], sphereT1[2]);
-	glPopMatrix();
-	
-	glEnable(GL_BLEND);
-	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glPushMatrix();
-		glColor4f(LARANJA);
-		glRotatef(angRot,0,1,0);
-		glTranslatef(sphereP2[0], sphereP2[1], sphereP2[2]);
-		glutSolidSphere(sphereT2[0], sphereT2[1], sphereT2[2]);
-	glPopMatrix();
-
+	if(angRot >= 360)
+		angRot -= 360;
+	else if(angRot < 0)
+		angRot += 360;
+	GLfloat tan = (obsP[0]/(GLfloat)raio)/(obsP[2]/(GLfloat)raio);
+	GLfloat angLook = atan(tan);
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Eixos
 	glColor4f(BLACK);
 	glBegin(GL_LINES);
@@ -99,7 +104,24 @@ void drawScene(){
 		glVertex3i( 0, 0, 0); 
 		glVertex3i( 0, 0,zC); 
 	glEnd();
-
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Sphere
+	glEnable(GL_BLEND);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glPushMatrix();
+		if(angRot > angLook+90 && angRot < angLook+270)
+			sphere2();
+		else
+			sphere1();
+	glPopMatrix();
+	
+	glEnable(GL_BLEND);
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glPushMatrix();
+		if(angRot > angLook+90 && angRot < angLook+270)
+			sphere1();
+		else
+			sphere2();
+	glPopMatrix();
 }
 
 void display(void){
@@ -129,9 +151,13 @@ void keyboard(unsigned char key, int x, int y){
     switch (key) {
     case 'T':
     case 't':
+    	if(alpha<1)
+    		alpha+=0.1;
 	    break;
 	case 'R':
     case 'r':
+    	if(alpha>0)
+    		alpha-=0.1;
 	    break;
 	
 	case 'A':
@@ -165,7 +191,7 @@ int main(int argc, char** argv){
 	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
 	glutInitWindowSize (wScreen, hScreen); 
 	glutInitWindowPosition (100, 100); 
-	glutCreateWindow ("Cenas 5");
+	glutCreateWindow ("TP5");
   
 	init();
 
